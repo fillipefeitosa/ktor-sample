fd8d5946cb7bfba7dd5a53c5627a1e3bdca5d745
Fillipe Feitosa <fillfeitosa@gmail.com>
2024-05-27 11:32:07 +0100

    First commit: fully working note endpoints

---
+++ src/main/kotlin/com/example/Application.kt
@@ -0,0 +1,35 @@
+package com.example
+
+import com.example.modules.configureNoteRouting
+import com.example.plugins.*
+import com.example.services.NoteService
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.plugins.contentnegotiation.*
+import org.jetbrains.exposed.sql.Database
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+            .start(wait = true)
+}
+
+fun Application.module() {
+    // configureSerialization()
+    // configureDatabases()
+    // configureRouting()
+
+    install(ContentNegotiation) { json() }
+
+    val database =
+            Database.connect(
+                    url = "jdbc:h2:file:./database/db;MODE=MYSQL;DB_CLOSE_DELAY=-1;",
+                    user = "root",
+                    driver = "org.h2.Driver",
+                    password = ""
+            )
+
+    val service = NoteService(database)
+    configureNoteRouting(service)
+}
---
+++ src/main/kotlin/com/example/models/Note.kt
@@ -0,0 +1,10 @@
+package com.example.models
+
+import com.example.responses.NoteResponse
+import java.util.*
+
+class Note(val id: UUID = UUID.randomUUID(), val title: String, val message: String) {
+  fun toNoteResponse(): NoteResponse {
+    return NoteResponse(id = id.toString(), title = title, message = message)
+  }
+}
